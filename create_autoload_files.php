c612b67c0a9dbe15855486206d3c18b5c5cd13d0
Keunbae Park <pkboom@users.noreply.github.com>
2022-01-29 00:00:55 -0500

    wip

--- create_autoload_files.php
+++ create_autoload_files.php
@@ -1,14 +1,5 @@
 <?php
 
-echo 'This will create a `autoload/files` in `composer.json`'.PHP_EOL;
-echo 'Make sure you are in a project. Ready?'.PHP_EOL;
-
-$f = fopen('php://stdin', 'r');
-
-$line = fgets($f);
-
-fclose($f);
-
 $root = getcwd();
 
 $path = "$root/composer.json";
